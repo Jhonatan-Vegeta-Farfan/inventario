f25bb66b0039ddcae0a1fc6c988e908a6a0054a8
jhonatan <jhonatannfarfan@gmail.com>
2025-08-26 12:32:46 -0500

    1

--- src/view/imprimir-instituciones.php
+++ src/view/imprimir-instituciones.php
@@ -1,12 +1,8 @@
 <?php
-    $ruta = explode("/", $_GET['views']);
-    if (!isset($ruta[1]) || $ruta== "") {
-        header("location:".BASE_URL."movimientos");
-    }
 
     $curl = curl_init(); //inicia la sesión cURL
     curl_setopt_array($curl, array(
-        CURLOPT_URL => BASE_URL_SERVER."src/control/Movimiento.php?tipo=buscar_movimiento_id&sesion=".$_SESSION['sesion_id']."&token=".$_SESSION['sesion_token']."&data=".$ruta[1], //url a la que se conecta
+        CURLOPT_URL => BASE_URL_SERVER."src/control/Institucion.php?tipo=buscar_instituciones&sesion=".$_SESSION['sesion_id']."&token=".$_SESSION['sesion_token'], //url a la que se conecta
         CURLOPT_RETURNTRANSFER => true, //devuelve el resultado como una cadena del tipo curl_exec
         CURLOPT_FOLLOWLOCATION => true, //sigue el encabezado que le envíe el servidor
         CURLOPT_ENCODING => "", // permite decodificar la respuesta y puede ser"identity", "deflate", y "gzip", si está vacío recibe todos los disponibles.
@@ -40,7 +36,7 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>Papeleta de Rotación de Bienes</title>
+  <title>Registro general de instituciones</title>
   <style>
     body {
       font-family: Arial, sans-serif;
@@ -97,79 +93,63 @@
   </style>
 </head>
 <body>
-<br><br><br>
-  <h2><i>Papeleta de Rotación de Bienes</i></h2>
+  <h2><i>LISTADO GENERAL DE INSTITUCIONES REGISTRADAS</i></h2>
 <br><br>
   <p>ENTIDAD <span class="subrayado">: DIRECCION REGIONAL DE EDUCACION - AYACUCHO</span></p>
   <p>ÁREA <span class="subrayado">: OFICINA DE ADMINISTRACIÓN</span></p>
-  <p>ORIGEN <span class="subrayado">: '. $respuesta->amb_origen->codigo.' - '.$respuesta->amb_origen->detalle.'</span></p>
-  <p>DESTINO <span class="subrayado">: '. $respuesta->amb_destino->codigo. ' - '. $respuesta->amb_destino->detalle .'</span></p>
+  <p>INFORME <span class="subrayado">:  Relación de Instituciones Registradas</span></p>
 
-  <p class="motivo">MOTIVO (*): <span class="subrayado">'. $respuesta->movimiento->descripcion.'</span></p>
+  <p class="motivo">DESCRIPCIÓN: <span class="subrayado"> Listado consolidado de instituciones registradas en el sistema institucional.</span></p>
 
   <table style="width: 100%; border-collapse: collapse; margin-top: 20px;" cellspacing="0" cellpadding="4">
     <thead>
       <tr>
         <th style="border: 0.3px solid #444; background-color: #f2f2f2;">ITEM</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">CÓDIGO PATRIMONIAL</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">NOMBRE DEL BIEN</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">MARCA</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">COLOR</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">MODELO</th>
-        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">ESTADO</th>
+        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">BENEFICIARIO</th>
+        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">CÓDIGO MODULAR</th>
+        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">RUC</th>
+        <th style="border: 0.3px solid #444; background-color: #f2f2f2;">DENOMINACIÓN</th>
       </tr>
     </thead>
     <tbody>
         
         ';
-     if (empty($respuesta->detalle)) {
+     if (empty($respuesta->instituciones)) {
       $contenido_pdf .= '<tr>
       <td colspan = "7" style="text-align: center; border: 0.3px solid #444; font-size: 12.5px; font-weight: 300; ">
-      No se encontraron bienes registrados para este movimiento
+      No se encontraron instituciones registradas.
       </td>
       </tr>
       ';
      } else {
         $contador = 1;
-foreach ($respuesta->detalle as $bien) {
+foreach ($respuesta->instituciones as $institucion) {
     $contenido_pdf .= '<tr>';
     $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$contador.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->cod_patrimonial.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->denominacion.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->marca.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->color.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->modelo.'</td>';
-    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$bien->estado_conservacion.'</td>';
+    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$institucion->beneficiario_nombre.'</td>';
+    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$institucion->cod_modular.'</td>';
+    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$institucion->ruc.'</td>';
+    $contenido_pdf .= '<td style="border: 0.3px solid #444;">'.$institucion->nombre.'</td>';
     $contenido_pdf .= '</tr>';
     $contador++;
 }
      }
      
-      
+// 1. Obtener la fecha actual
+date_default_timezone_set('America/Lima');
 
-$fecha_raw = $respuesta->movimiento->fecha_registro; // esta es la que te llega
+$fecha_obj = new DateTime(); // Fecha actual
 
-// 1. Convertir la fecha al formato correcto
-$fecha_obj = DateTime::createFromFormat('Y-m-d H:i:s', $fecha_raw);
-
-// 2. Array de meses en español
+// 2. Array de meses
 $meses = [
     1 => "enero", "febrero", "marzo", "abril", "mayo", "junio",
     "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
 ];
 
-// 3. Verificamos que se haya parseado bien
-if ($fecha_obj instanceof DateTime) {
-    $dia = $fecha_obj->format('j');
-    $mes = (int)$fecha_obj->format('n');
-    $anio = $fecha_obj->format('Y');
-
-    // 4. Formatear la fecha
-    $fecha_formateada = "$dia de " . $meses[$mes] . " de $anio";
-} else {
-    $fecha_formateada = "Fecha inválida";
-}
-
+// 3. Formatear la fecha
+$dia = $fecha_obj->format('j');
+$mes = (int)$fecha_obj->format('n');
+$anio = $fecha_obj->format('Y');
 
 $contenido_pdf .= '
 
@@ -177,7 +157,7 @@ $contenido_pdf .= '
   </table>
 
   <p style="text-align: right; padding-right: 40px;">
-  Ayacucho, '.$dia.' de '.$meses[$mes].' de '.$anio.'
+  Ayacucho, '.$dia.' de '.$meses[$mes].' del '.$anio.'
 </p>
  <table style="width: 100%; border: none;" cellspacing="0" cellpadding="0">
     <tr>
@@ -186,7 +166,7 @@ $contenido_pdf .= '
     <tr>
       <td style="width: 50%; text-align: center;">
         ------------------------------<br>
-        ENTREGUE CONFORME
+        ENTREGUÉ CONFORME
       </td>
       <td style="width: 50%; text-align: center;">
         ------------------------------<br>
@@ -234,8 +214,8 @@ $pdf = new MYPDF();
 
 
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Jhonatan Farfan');
-$pdf->SetTitle('Reporte de movimientos');
+$pdf->SetAuthor('Renzo Gamboa');
+$pdf->SetTitle('Listado general de instituciones');
 
 //asignar márgenes
 $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
@@ -244,15 +224,17 @@ $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 //Va por secciones si quieres utilizar distintas fuentes(tipo de fuente, grosor, tamaño de la letra)
 $pdf->SetFont('courier', '', 9);
+$pdf->SetAutoPageBreak(true, 20); // activa salto de página con 20 de margen inferior
+$pdf->SetMargins(10, 40, 10); // (izquierda, arriba, derecha)
+ // margen para el footer
+
 // add a page
-$pdf->AddPage();
+$pdf->AddPage('P');
 
 // output the HTML content
 $pdf->writeHTML($contenido_pdf);
 
 //Close and output PDF document
-$pdf->Output('reporte de movimientos.pdf', 'I');
+$pdf->Output('Listado de instituciones.pdf', 'I');
 
     }
-    
-    
